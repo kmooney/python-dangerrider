18d3db9438bb531f176633f9d64b328d0649b5c4
Kevin Mooney <kevin@cukerinteractive.com>
2012-07-04 15:58:10 -0500

    Added RangeIndex

--- dangerrider/core.py
+++ dangerrider/core.py
@@ -9,20 +9,31 @@ class Table(object):
     aggregators = [] 
 
     def refresh_indices(self):
-        #TODO Implement refresh indices
-        pass
+        # clear all the indices
+        for index in self.indices.itervalues():
+            index.clear()
 
+        # update all the indices, for each item
+        for obj in self.object_list:
+            self.update_indices(obj)
+
+    def update_indices(self, obj):
+        for index in self.indices.itervalues():
+            index.update(obj)
+        
     def add_object(self, obj):
         self.object_list.append(obj)
 
-        for index in self.indices.itervalues():
-            index.update(obj)
+        self.update_indices(obj)
 
         for aggregator in self.aggregators:
             aggregator.update(obj)
 
     def add_index(self, index):
         self.indices[tuple(index.properties)] = index
+
+    def add_aggregator(self, aggregator):
+        self.aggregators[aggregator.name] = aggregator
 
     def filter(self, key_tuple, value_tuple):
         if len(key_tuple) != len(value_tuple):
@@ -42,12 +53,19 @@ class Table(object):
             if include:
                 rvalue.append(obj) 
             
+    def metadata(self):
+        return {'indices': self.indices, 
+                'aggregators': self.aggregators}        
+
     def __init__(self):
         self.object_list = []
         self.indices = {} 
         self.aggregators = []
 
 class StorageMixin(object):
+    """
+        Use this mixin to make your objects compatible with the dangerrider table.
+    """
     def get_index_values(self, index):
         return tuple([getattr(self,prop,None) for prop in index.properties])
         
@@ -60,15 +78,57 @@ class Index(object):
     properties = [ ]
     lookup = { } 
 
+    def __init__(self, name, properties = []):
+        self.name = name
+        self.properties = properties
+
     def update(self, obj):
         index_values = obj.get_index_values( self )
         if index_values not in self.lookup:
             self.lookup[index_values] = [] 
         self.lookup[index_values].append( obj )
+    
+    def set_properties(self, props):
+        self.properties = props 
+
+    def clear(self):
+        del self.lookup
+        self.lookup = { } 
 
     def get_objects(self, index_tuple):
         return self.lookup[index_tuple]
 
+class RangeIndex(object):
+    """
+        A range index refers to properties that fall within a range.  Range indexes
+        can only index a single property (how to tell if a tuple is greater or less 
+        than another?)  Range Indexes are not too useful for objects or words, but are
+        super useful for numbers.  
+    """ 
+    def set_properties(self, props):
+        if len(props) > 1:
+            raise Exception ("Range Index Must be on a single property")
+        super(RangeIndex, self).set_properties(props)
+        
+
+    def update(self, obj):
+        index_values = obj.get_index_values( self )
+        if len(index_values) > 1:
+            raise Exception ("Range Index Must be on a single value")
+        super(RangeIndex, self).update(obj)
+
+    def get_objects(self, index_tuple):
+        if len(index_tuple) > 1:
+            raise Exception ("Range Index Must be on a single value")
+        return super(RangeIndex, self).get_objects(index_tuple)
+    
+    def get_objects_by_range(self, range_start, range_end=None):
+        keys = sorted(list(self.properties.iterkeys()))
+        if range_end != None:
+            return [self.lookup[key] for key in keys if key >= range_start and key <= range_end]
+        else:
+            return [self.lookup[key] for key in keys if key >= range_start]
+    
 class Aggregator(object):
     """
         An aggregator contains precalculated values for things like sums, averages, 
